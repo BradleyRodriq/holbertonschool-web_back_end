994dff430e2c46c64b24e7dd6c19a0c5e73ff3da
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-04-22 16:44:53 -0400

    pycode fixes

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """duck type an iterable object"""
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """element length"""
     return [(i, len(i)) for i in lst]
