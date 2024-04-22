c0df7e0adb5cf7e398020bd972b36ad275fed2e5
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-04-22 14:09:59 -0400

    finished task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""list sum"""
+from typing import List
+
+
+def sum_list(float_list: List[float]) -> float:
+    """returns sum of all list items"""
+    return sum(float_list)
