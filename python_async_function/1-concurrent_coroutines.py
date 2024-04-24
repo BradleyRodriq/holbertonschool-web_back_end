2cadfdfaef8c0ed532d786b36ab9039ae3a5c949
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-04-24 02:14:22 -0400

    task 4 and task 1 pycode fix

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ from typing import List
 randwait = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay:int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """wait n"""
     routines = [randwait(max_delay) for i in range(n)]
     return [await x for x in asyncio.as_completed(routines)]
