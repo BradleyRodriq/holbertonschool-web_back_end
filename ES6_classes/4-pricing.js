a2ebbb398bd245bf79991dbe8f115065b7569c7d
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-05-09 00:00:39 -0400

    task 4, pricing and currency lint fixes

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -7,7 +7,7 @@ export default class Pricing {
   }
 
   get amount() {
-    return this._amount
+    return this._amount;
   }
 
   set amount(amount) {
@@ -15,7 +15,7 @@ export default class Pricing {
   }
 
   get currency() {
-    return this._currency
+    return this._currency;
   }
 
   set currency(currency) {
@@ -23,7 +23,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
