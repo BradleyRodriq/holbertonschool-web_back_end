8b6c5dbfdd79b2a24bb536e04794256b2404f29b
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-05-21 10:57:51 -0400

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+const getListStudents = () => [
+    {
+        id: 1,
+        firstName: 'Guillaume',
+        location: 'San franciso',
+    },
+    {
+        id: 2,
+        firstName: 'James',
+        location: 'Columbia'
+    },
+    {
+        id: 5,
+        firstName: 'Serena',
+        location: 'San Franciso'
+    },
+];
+
+export default getListStudents;
