3f67b7443b05b83cbfc82ea13ac6cd4ae2fce12b
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-09-18 14:05:07 -0400

    task 1

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/user/bin/env python3
+""" 1-fifo_cache.py """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO cache class """
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.keys_queue = []
+        
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.keys_queue.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            OLDEST_KEY = self.keys_queue.pop(0)
+            del self.cache_data[OLDEST_KEY]
+            print(f"DISCARD: {OLDEST_KEY}")
+        self.cache_data[key] = item
+        self.keys_queue.append(key)
+            
+    def get(self, key):
+        """ Get an item by key """
+        return self.cache_data.get(key, None)
