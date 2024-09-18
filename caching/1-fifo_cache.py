4af8e8f227fb744c1e8ac748e2524cb709be14cb
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-09-18 14:12:58 -0400

    changed it a bit

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -25,4 +25,6 @@ class FIFOCache(BaseCaching):
             
     def get(self, key):
         """ Get an item by key """
-        return self.cache_data.get(key, None)
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
