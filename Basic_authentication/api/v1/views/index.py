b103f9e7eec7cd7b2bdef5825a1d2e799848a8a3
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-10-06 12:45:10 -0400

    unauthorized path

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+  
+  
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """
+    This path was made to test
+    the 401 error + Flask.
+    """
+    abort(401)
