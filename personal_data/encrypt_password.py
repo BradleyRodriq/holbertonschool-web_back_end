fa257895575942be46c1a7edda76475f92ac2803
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-10-06 11:52:02 -0400

    task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,3 +10,9 @@ def hash_password(password: str) -> bytes:
     _summary_
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    _summary_
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
