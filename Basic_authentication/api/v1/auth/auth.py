ea8d31aca2121374824180b2971e139f1af532c3
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-10-06 13:48:01 -0400

    task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,19 @@ class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth"""
-        return False
+        if path is None:
+            return True
+        
+        if excluded_paths is None or not excluded_paths:
+            return True
+        
+        if path in excluded_paths:
+            return False
+        
+        if not path.endswith('/'):
+            path += '/'
+        
+        return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """authorization_header"""
