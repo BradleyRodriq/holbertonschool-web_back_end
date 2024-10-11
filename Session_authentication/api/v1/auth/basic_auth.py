eacb75d2ec79acd91047f4c4e1f5e569abf5ff96
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-10-10 21:08:51 -0400

    basic_auth fixes

--- Session_authentication/api/v1/auth/basic_auth.py
+++ Session_authentication/api/v1/auth/basic_auth.py
@@ -3,12 +3,11 @@
 Contains 'BasicAuth', which is an empty
 child class of 'Auth'.
 """
-from api.v1.auth.auth import Auth
-from typing import TypeVar, List
 import base64
+from typing import Tuple, Optional
 import binascii
+from api.v1.auth.auth import Auth
 from models.user import User
-from models.base import DATA
 
 
 class BasicAuth(Auth):
@@ -19,16 +18,24 @@ class BasicAuth(Auth):
         self,
         authorization_header: str
     ) -> str:
-        HEADER_START = "Basic "
+        """_summary_
+
+        Args:
+            authorization_header (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        head = "Basic "
 
         if authorization_header is None:
             return None
-        if type(authorization_header) != str:
+        if not isinstance(authorization_header, str):
             return None
-        if not authorization_header.startswith(HEADER_START):
+        if not authorization_header.startswith(head):
             return None
 
-        return authorization_header[len(HEADER_START):]
+        return authorization_header[len(head):]
 
     def decode_base64_authorization_header(
             self,
@@ -38,23 +45,23 @@ class BasicAuth(Auth):
         """
         if base64_authorization_header is None:
             return None
-        if type(base64_authorization_header) != str:
+        if not isinstance(base64_authorization_header, str):
             return None
 
         try:
             return base64.b64decode(base64_authorization_header).decode()
-        except Exception:
+        except (binascii.Error, ValueError, UnicodeDecodeError):
             return None
 
     def extract_user_credentials(
             self,
-            decoded_base64_authorization_header: str) -> (str, str):
+            decoded_base64_authorization_header: str) -> Tuple[str, str]:
         """
         Extracts the user credentials
         """
         if decoded_base64_authorization_header is None:
             return (None, None)
-        if type(decoded_base64_authorization_header) != str:
+        if not isinstance(decoded_base64_authorization_header, str):
             return (None, None)
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
@@ -66,14 +73,14 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self,
             user_email: str,
-            user_pwd: str) -> TypeVar('User'):
+            user_pwd: str) -> Optional[User]:
         """
         Returns the User object
         """
 
-        if user_email is None or type(user_email) != str:
+        if user_email is None or not isinstance(user_email, str):
             return None
-        if user_pwd is None or type(user_pwd) != str:
+        if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
         try:
@@ -81,28 +88,27 @@ class BasicAuth(Auth):
                 if user.email == user_email and \
                         user.is_valid_password(user_pwd):
                     return user
-        except KeyError as e:
+        except KeyError:
             pass
 
         return None
 
-    def current_user(self, request: str = None) -> TypeVar('User'):
+    def current_user(self, request=None) -> Optional[User]:
         """
         Returns the User object
         """
-
-        BASE_64_AUTH_HEADER = self.extract_base64_authorization_header(
+        base_64_auth_header = self.extract_base64_authorization_header(
             request
         )
 
-        AUTH_HEADER = self.decode_base64_authorization_header(
-            BASE_64_AUTH_HEADER
+        auth_header = self.decode_base64_authorization_header(
+            base_64_auth_header
         )
 
-        USER_CREDENTIALS = self.extract_user_credentials(
-            AUTH_HEADER
+        user_credentials = self.extract_user_credentials(
+            auth_header
         )
 
-        RESULT = self.user_object_from_credentials(*USER_CREDENTIALS)
+        result = self.user_object_from_credentials(*user_credentials)
 
-        return RESULT
+        return result
