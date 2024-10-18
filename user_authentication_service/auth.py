7983f492eb6b5843d49d53355c39edf7e4d35832
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-10-18 09:48:25 -0400

    task 4

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+auth
+"""
+import bcrypt
+from db import DB
+from user import User
+
+def _hash_password(password: str) -> bytes:
+    """
+    hash password using bcrypt
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
