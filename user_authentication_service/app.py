2e60656ef8f3108cde31af69024fa345a9b47c9e
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-10-18 10:18:24 -0400

    task 6

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,19 +2,36 @@
 """
 Flask app for signing 'User's in.
 """
-from typing import Tuple
+from typing import Tuple, Optional
 import flask
+from auth import Auth
+
 
 app = flask.Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def bienvenue() -> Tuple[flask.Response, int]:
     """
-    Expects GET with nothing,
-    responds with {"message": "Bienvenue"} and a status code of 200.
+    responds with Bienevue
     """
     return flask.jsonify({"message": "Bienvenue"}), 200
+
+
+def users() -> Tuple[flask.Response, int]:
+    """
+    users route
+    """
+    email: Optional[str] = flask.request.form.get("email")
+    password: Optional[str] = flask.request.form.get("password")
+
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return flask.jsonify({"message": "email already registered"}), 400
+    else:
+        return flask.jsonify({"email": email, "message": "user created"}), 200
 
 
 if __name__ == "__main__":
