da429be0c67dfee3498c9a18ad36db1962f8afab
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-10-25 01:44:08 -0400

    task 3 and 4

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,54 +1,43 @@
 #!/usr/bin/env python3
-"""
-basic app
-"""
-from typing import Union
-from os import environ
-import flask
-import flask_babel
-import babel
+""" Route module for the API - Force locale with URL parameter """
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from os import getenv
+
+app = Flask(__name__)
+babel = Babel(app)
 
 
+class Config(object):
+    """ Setup - Babel configuration """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-class Config:
+
+app.config.from_object('4-app.Config')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return: 4-index.html
     """
-    babel config
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app = flask.Flask(__name__)
-app.config.from_object(Config)
-
+    return render_template('4-index.html')
 
 
 @babel.localeselector
-def get_locale() -> Union[str, None]:
-    """
-    get_locale
-    """
-    locale_arg: Union[str, None] = flask.request.args.get("locale", None)
-
-    if locale_arg is not None and locale_arg in app.config["LANGUAGES"]:
-        return locale_arg
-
-    return flask.request.accept_languages.best_match(
-        app.config["LANGUAGES"]
-    )
-
-babel = flask_babel.Babel(app, locale_selector=get_locale)
-
-@app.route("/", strict_slashes=False)
-def home() -> flask.Response:
-    """
-    homepage
-    """
-    return flask.render_template("4-index.html")
+def get_locale() -> str:
+    """ Determines best match for supported languages """
+    if request.args.get('locale'):
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run(
-        environ.get("HOST"), environ.get("PORT")
-    )
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
