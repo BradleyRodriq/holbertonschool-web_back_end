0243999b10186f89ceb142264181cedb67b8726d
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-11-06 18:03:18 -0400

    task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Cache class """
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
 import redis
 
 
@@ -20,3 +20,26 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None):
+        """
+        gets the value for key
+        """
+        result = self._redis.get(key)
+
+        if fn is not None:
+            result = fn(result)
+
+        return result
+
+    def get_str(self, key):
+        """
+        returns string value
+        """
+        return self.get(key, lambda x: x.decode())
+
+    def get_int(self, key):
+        """
+        returns int value
+        """
+        return self.get(key, lambda x: int(x.decode()))
