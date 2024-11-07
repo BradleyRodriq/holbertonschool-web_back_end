f520579f1d9294c1e587b13052750416a509c4a0
BradleyRodriq <bradley.rodriguez2@yahoo.com>
2024-11-07 06:45:28 -0400

    task 3 and 4

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Cache class """
 from uuid import uuid4
-from typing import Union, Optional, Callable
+from typing import Union, Optional, Callable, List
 from functools import wraps
 import redis
 
@@ -15,6 +15,50 @@ def count_calls(method: Callable) -> Callable:
         self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
     return increment
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    keeps track of input and output
+    """
+    @wraps(method)
+    def log(self, *args, **kwargs):
+        inputs = args
+        output = method(self, *args, **kwargs)
+
+        self._redis.rpush(
+            f"{method.__qualname__}:inputs", str(inputs)
+        )
+
+        self._redis.rpush(
+            f"{method.__qualname__}:outputs", str(output)
+        )
+
+        return output
+    return log
+
+
+def replay(method: Callable) -> None:
+    """
+    methods history
+    """
+    redis_tunnel = redis.Redis()
+
+    inputs: List[bytes] = \
+        redis_tunnel.lrange(f"{method.__qualname__}:inputs", 0, -1)
+    outputs: List[bytes] = \
+        redis_tunnel.lrange(f"{method.__qualname__}:outputs", 0, -1)
+    call_count: int = int(redis_tunnel.get(f"{method.__qualname__}"))
+
+    print(f"{method.__qualname__} was called {call_count} times:")
+
+    for input, output in zip(inputs, outputs):
+        input = input.decode()
+        output = output.decode()
+
+        print(
+            f"{method.__qualname__}(*{input}) -> {output}"
+        )
 
 
 class Cache:
